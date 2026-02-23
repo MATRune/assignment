fe4d112ea496ec6f5a92d775356309b1eb6b1f52
MATRune <malik.thomas.200531@gmail.com>
2026-02-23 18:31:42 -0400

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -71,13 +71,13 @@ def initialize():
 @cli.command()
 def test():
     try:
-        subprocess.run(["npm", "install"], check=True)
+        subprocess.run(["npm.cmd", "install"], check=True)
     except subprocess.CalledProcessError:
         typer.secho("Installing test package failed. Install Node/npm on your PC to continue", fg=typer.colors.RED)
         raise typer.Exit(code=1)
         
     try:
-        subprocess.run(["npm", "test"], check=True)
+        subprocess.run(["npm.cmd", "test"], check=True)
     except subprocess.CalledProcessError:
         typer.secho("Tests failed!", fg=typer.colors.RED)
         raise typer.Exit(code=1)
