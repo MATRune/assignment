7eac57bdcfb90307c223dc3a3ee6dd3e800acd02
MATRune <malik.thomas.200531@gmail.com>
2026-02-23 19:40:04 -0400

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -5,6 +5,7 @@ from app.database import create_db_and_tables, get_cli_session, drop_all
 from app.models import *
 from app.auth import encrypt_password
 import subprocess
+import platform
 
 cli = typer.Typer()
 
@@ -71,13 +72,13 @@ def initialize():
 @cli.command()
 def test():
     try:
-        subprocess.run(["npm.cmd", "install"], check=True)
+        subprocess.run(["npm", "install"], check=True, shell=platform.system() == "Windows")
     except subprocess.CalledProcessError:
         typer.secho("Installing test package failed. Install Node/npm on your PC to continue", fg=typer.colors.RED)
         raise typer.Exit(code=1)
         
     try:
-        subprocess.run(["npm.cmd", "test"], check=True)
+        subprocess.run(["npm", "test"], check=True, shell=platform.system() == "Windows")
     except subprocess.CalledProcessError:
         typer.secho("Tests failed!", fg=typer.colors.RED)
         raise typer.Exit(code=1)
